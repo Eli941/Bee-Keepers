5a1544ebfef7adf543233fe9e2620010cf2af3a6
Eli941 <Zerais.149@gmail.com>
2024-09-24 18:30:13 +0100

    Add files via upload

--- bee.py
+++ bee.py
@@ -5,6 +5,9 @@ TITLE = "Bee Keeping"
 WIDTH = 500
 HEIGHT = 500
 
+score = 0
+game_over = False
+
 bee = Actor("bee")
 flower = Actor("flower")
 
@@ -17,6 +20,44 @@ def draw():
     
     bee.draw()
     flower.draw()
+    screen.draw.text("Score:" + str(score), color="blue", topleft=(10,10))
+
+    if game_over:
+        screen.fill("pink")
+        screen.draw.text("Time's Up! Your Final Score: " + str(score), midtop= (WIDTH/2,10), fontsize=40, color="red")
+def place_flower():
+    flower.x = randint(70,(WIDTH-70))
+    flower.y = randint(70,(WIDTH-70))
+
+def time_up():
+    global game_over
+    game_over = True
+
+def update():
+    global score 
+    if keyboard.left:
+         bee.x = bee.x -2
+    if keyboard.right:
+         bee.x = bee.x +2
+    if keyboard.up:
+         bee.y = bee.y -2
+    if keyboard.down:
+         bee.y = bee.y +2
+
+    flower_collected = bee.colliderect(flower)
+
+    if flower_collected:
+        score = score + 10
+        place_flower()
+
+
+clock.schedule(time_up,60.0)
+
+
+
+
+
+
 
 
 pgzrun.go()
